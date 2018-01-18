139137ca1d722331a542cb94e383e3d1968b9bbc
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-01-18 11:39:38 -0600

    demo_12: use concatMap() operator and concat() function

--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ import { marbleLogTo } from './console-log';
 import { Scheduler } from 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
 
-import { forkJoin }    from 'rxjs/observable/forkJoin';
+import { concat }    from 'rxjs/observable/concat';
 import { defer }    from 'rxjs/observable/defer';
 import { interval } from 'rxjs/observable/interval';
 import { timer }    from 'rxjs/observable/timer';
 import { from }     from 'rxjs/observable/from';
+import { fromEvent} from 'rxjs/observable/fromEvent';
 import { of }       from 'rxjs/observable/of';
 
 import {
-  switchMap, map, concat, flatMap, merge,
-  startWith, filter, takeWhile, bufferCount,
+  switchMap, map, flatMap, merge,
+  startWith, filter, takeWhile, bufferCount, take,
   tap, concatMap, delay, debounceTime, distinctUntilChanged
 } from 'rxjs/operators';
 
@@ -29,4 +30,19 @@ let obs3$ = of(5, 6);
 
 // Emit most-recent value of each observable...
 // grouped as a single emitted value
-forkJoin(obs1$, obs2$, obs3$).subscribe( observer );
+concat([obs1$, obs2$, obs3$])
+  .pipe(
+    flatMap(x => x)
+  )
+  .subscribe( observer )
+
+
+// Show how concatMap() can be used...
+var target = document.getElementById("btnStart");
+var clicks = fromEvent(target, 'click');
+    clicks.pipe(
+      concatMap(ev => interval(1000).pipe(
+          take(4)
+      ))
+    )
+    .subscribe( observer );
