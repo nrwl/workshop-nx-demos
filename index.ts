30fcf24f20cb0f52d1b47a37c14f72295c604e10
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-01-18 11:33:17 -0600

    demo_2: emit next + error

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-import { logTo, marbleLogTo } from './console-log';
+import { marbleLogTo } from './console-log';
 
 // RxJS Imports
+import { Scheduler } from 'rxjs/Rx';
 import { Observable } from "rxjs/Observable";
+
 import { defer }    from 'rxjs/observable/defer';
 import { interval } from 'rxjs/observable/interval';
 import { timer }    from 'rxjs/observable/timer';
 import { from } from 'rxjs/observable/from';
 import { of }       from 'rxjs/observable/of';
+
 import {
-  switchMap, map, concat, startWith, filter, takeWhile, bufferCount, tap, concatMap, delay
+  switchMap, map, concat,
+  startWith, filter, takeWhile, bufferCount,
+  tap, concatMap, delay
 } from 'rxjs/operators';
 
 // ************************************
@@ -19,10 +24,10 @@ const myObs$ = Observable.create( observer => {
 
   observer.next(1);
   observer.next(2);
-  observer.next(3);
+  observer.error('oops');
 
   observer.complete();
 
 });
 
-myObs$.subscribe( logTo("logOutput") );
+myObs$.subscribe( marbleLogTo("logOutput") );
